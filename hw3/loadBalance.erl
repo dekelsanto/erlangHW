d92f1bd2198c33aa0d36cc81c29ccf3436d2e5d7
Dekel <dekel@inspiron15>
2019-06-19 15:42:52 +0300

    Apparently this is functional now...

--- hw3/loadBalance.erl
+++ hw3/loadBalance.erl
@@ -2,7 +2,7 @@
 -behaviour(supervisor).
 
 -export([init/1, startServers/0, stopServers/0]).
--export([numberOfRunningFunctions/1]).
+-export([numberOfRunningFunctions/1, calcFun/3]).
 init([]) -> 
     Server1 = {server1, {serverInstance, start_link, [server1]},
         permanent, 2000, worker, []},
@@ -26,3 +26,21 @@ numberOfRunningFunctions(Id) ->
         3 -> server3
     end, 
     gen_server:call(ServerName, {count}).
+
+calcFun(Pid, Fun, MsgRef) ->
+    TargetServer = selectServer([server1, server2, server3]),
+    gen_server:cast(TargetServer, {runFunc, {Pid, Fun, MsgRef}}).
+
+
+selectServer([DefaultServer | MoreServers]) -> 
+    selectServer(MoreServers, DefaultServer, gen_server:call(DefaultServer, {count})).
+
+selectServer([], CurrentServer, _CurrentFuncCount) -> CurrentServer;
+selectServer(_MoreServers, CurrentServer, 0) -> CurrentServer;
+selectServer([CandidateServer | MoreServers], CurrentServer, CurrentFuncCount) ->
+    CandidateFuncCount = gen_server:call(CandidateServer, {count}),
+    {WinnerServer, WinnerFuncCount} = case CandidateFuncCount < CurrentFuncCount of
+        true -> {CandidateServer, CandidateFuncCount};
+        false -> {CurrentServer, CurrentFuncCount}
+    end, 
+    selectServer(MoreServers, WinnerServer, WinnerFuncCount).
--- hw3/serverInstance.erl
+++ hw3/serverInstance.erl
@@ -4,6 +4,7 @@
 %% API
 -export([stop/1, start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([worker/4]).
 -record(state, {name, functionCount}).
 
 stop(Name) ->
@@ -12,8 +13,8 @@ stop(Name) ->
 start_link(Name) ->
    gen_server:start_link({local, Name}, ?MODULE, [Name], []).
 
-init(Name) ->
-    io:format("~p starting~n", Name),
+init([Name]) ->
+    io:format("~p starting~n", [Name]),
    {ok, #state{name=Name, functionCount=0}}.
 
 handle_call(stop, _From, State) ->
@@ -24,6 +25,16 @@ handle_call({count}, _From, State) ->
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
+
+handle_cast({runFunc, {Pid, Fun, MsgRef}}, State) ->
+    spawn(?MODULE, worker, [Pid, Fun, MsgRef, State#state.name]),
+    NewState = State#state{functionCount = State#state.functionCount + 1},
+    {noreply, NewState};
+
+handle_cast({funcEnd}, State) ->
+    NewState = State#state{functionCount = State#state.functionCount - 1},
+    {noreply, NewState};
+    
 
 handle_cast(_Msg, State) ->
    {noreply, State}.
@@ -38,7 +49,10 @@ code_change(_OldVsn, State, _Extra) ->
    {ok, State}.
 
 
-% worker(Pid, )
+worker(Pid, Fun, MsgRef, ServerName) -> 
+    Result = Fun(), 
+    Pid ! {MsgRef, Result}, 
+    gen_server:cast(ServerName, {funcEnd}).
 
 
 
