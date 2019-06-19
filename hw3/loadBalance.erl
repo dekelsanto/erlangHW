fbd281ee3a35761c8475c4f132222dc646f3a382
Dekel <dekel@inspiron15>
2019-06-19 15:07:05 +0300

    Added .pdf assignment paper and basic solution structure

---
+++ hw3/loadBalance.erl
@@ -0,0 +1,5 @@
+-module(loadBalance).
+-behaviour(supervisor).
+
+% -export([startServers/0, stopServers/0]).
+init([]) -> ok.
---
+++ hw3/serverInstance.erl
@@ -0,0 +1,39 @@
+-module(serverInstance).
+-behaviour(gen_server).
+
+%% API
+-export([stop/1, start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-record(state, {name, functionCount}).
+
+stop(Name) ->
+   gen_server:call(Name, stop).
+
+start_link(Name) ->
+   gen_server:start_link({local, Name}, ?MODULE, [Name], []).
+
+init(Name) ->
+   {ok, #state{name=Name, functionCount=0}}.
+
+handle_call(stop, _From, State) ->
+   {stop, normal, stopped, State};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+   {noreply, State}.
+
+handle_info(_Info, State) ->
+   {noreply, State}.
+
+terminate(_Reason, _State) ->
+   ok.
+
+code_change(_OldVsn, State, _Extra) ->
+   {ok, State}.
+
+
+
+
+
