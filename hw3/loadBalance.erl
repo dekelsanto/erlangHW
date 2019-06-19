d83bcbd4c7c9f11d9650aac71325bcb7ff2f391a
Dekel <dekel@inspiron15>
2019-06-19 15:17:31 +0300

    Functional start/stop servers

--- hw3/loadBalance.erl
+++ hw3/loadBalance.erl
@@ -1,5 +1,19 @@
 -module(loadBalance).
 -behaviour(supervisor).
 
-% -export([startServers/0, stopServers/0]).
-init([]) -> ok.
+-export([init/1, startServers/0, stopServers/0]).
+init([]) -> 
+    Server1 = {server1, {serverInstance, start_link, [server1]},
+        permanent, 2000, worker, []},
+    Server2 = {server2, {serverInstance, start_link, [server2]},
+        permanent, 2000, worker, []},
+    Server3 = {server3, {serverInstance, start_link, [server3]},
+        permanent, 2000, worker, []},
+    {ok, {{rest_for_one, 1, 1}, [Server1, Server2, Server3]}}.
+
+startServers() ->
+    make:all([load]),
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+stopServers() ->
+    exit(whereis(?MODULE), normal).
